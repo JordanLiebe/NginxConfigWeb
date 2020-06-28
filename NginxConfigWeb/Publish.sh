1105568fbd7d50ddf079399b52aa87e74b9e4c16
Jordan M. Liebe <jordan.liebe@outlook.com>
2020-06-28 13:29:50 -0700

    Removed .vs file and added the Publish script

---
+++ NginxConfigWeb/Publish.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/bash
+# Script to Publish this Dotnet App.
+
+echo "Stopping Service..."
+
+systemctl stop NginxConfigWeb.service
+
+sleep 3
+
+dotnet publish -o /usr/local/NginxConfigWeb/
+
+sleep 3
+
+echo "Restarting Service..."
+
+systemctl start NginxConfigWeb.service
